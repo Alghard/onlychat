9131d1c2e05ff862488b9b4f2206121ccd3116ad
Alghard <julien.varela@gmail.com>
2025-04-03 18:30:03 +0200

    part 2 of django tutorial

--- testsite/mysite/settings.py
+++ testsite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'visioconf.apps.VisioconfConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
---
+++ testsite/visioconf/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def index(request):
+  return HttpResponse("C'est ici que je vais travailler sur la visio conf de mon projet perso")
